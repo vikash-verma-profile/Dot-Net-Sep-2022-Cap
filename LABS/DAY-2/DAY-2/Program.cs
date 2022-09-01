2a225465c1020cebe232b96716e0ef8f47550b7e
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-09-01 13:16:12 +0530

    added git commands

---
+++ LABS/DAY-2/DAY-2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DAY_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+
+            //<data-type> variable-name operator value
+            Console.WriteLine(a);
+        }
+    }
+}
