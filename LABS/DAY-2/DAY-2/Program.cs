adb982299e40bd66d12da615f72784626f79aaa0
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-09-01 14:35:11 +0530

    added conditional statements

--- LABS/DAY-2/DAY-2/Program.cs
+++ LABS/DAY-2/DAY-2/Program.cs
@@ -7,9 +7,29 @@ namespace DAY_2
         static void Main(string[] args)
         {
             int a = 5;
+            int b = 71;
+            int c = 36;
+
+            Console.WriteLine(c%2);
+            Console.WriteLine(c/2);
+
+            //conditional statements
+            //if else ,switch
 
             //<data-type> variable-name operator value
-            Console.WriteLine(a);
+            Console.WriteLine(a+b);
+
+            if (a > 7)
+            {
+                Console.WriteLine("a is greater than 7");
+            }
+            else
+            {
+                Console.WriteLine("7 is greater than a");
+            }
+            Console.WriteLine("Please enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Value entered by you is " + name);
         }
     }
 }
