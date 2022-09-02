35b6b0d4072b5883a28b9059c9757e947be51bc0
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-09-02 09:56:44 +0530

    added code for type casting

---
+++ LABS/Day-3/Day-3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Day_3
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// 
+    //Single line comment
+
+    /*
+     * 
+     * 
+     */
+    //switch  and type casting
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int a = 5;//4 bytes
+            double b = a;//Promotion 8 bytes implicit
+
+            double c = 3.25;
+            a = (int)c;//demotion and this is explicit conversion
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            //Convert class
+            //int.parse
+            //int.tryparse
+            Console.WriteLine("Please enter some number");
+            int Number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input value given by user is "+Number1);
+        }
+    }
+}
