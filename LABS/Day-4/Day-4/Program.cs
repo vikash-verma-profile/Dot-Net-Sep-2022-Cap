05c25d255bce09c615ebd4532a13c8a494c39409
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-09-05 10:12:08 +0530

    first commit

---
+++ LABS/Day-4/Day-4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Day_4
+{
+    /// <summary>
+    /// 
+    /// 2d array
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[2, 2];// { { 1, 2 },{ 3, 4 } };
+             //Input from a 2d array
+            for (int row = 0; row < 2; row++)
+            {
+                for (int col = 0; col < 2; col++)
+                {
+                    Console.Write("Please enter value for matrix["+row+","+col+"] :");
+                    matrix[row, col]=Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            //print a 2d array
+            for (int row = 0; row < 2; row++)
+            {
+                for (int col = 0; col < 2; col++)
+                {
+                    Console.Write(matrix[row, col] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
