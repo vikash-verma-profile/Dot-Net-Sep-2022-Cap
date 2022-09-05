1149258d8860e523ccfa3fe30ef91411ee5f98db
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-09-05 13:11:04 +0530

    code

--- LABS/Day-4/Day-4/Class3.cs
+++ LABS/Day-4/Day-4/Class3.cs
@@ -6,13 +6,20 @@ namespace Day_4
 {
     class Class3
     {
-        public static void Main()
+        public static void Main4()
         {
             string s = "Vikash";
             for (int i = 0; i < s.Length; i++)
             {
                 Console.WriteLine(s[i]);
             }
+            string s1=string.Concat(s, " ", "Verma");
+            string newstring = "Hi I am here";
+            string q=newstring.Replace("Hi","Hello");
+
+            
+            Console.WriteLine("Old string is "+newstring);
+            Console.WriteLine("After repacement new string is "+q);
         }
     }
 }
---
+++ LABS/Day-4/Day-4/Class4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_4
+{
+    class Student
+    {
+        //members or fields
+        //methods
+        //constrtuctors and destructors
+        //properties
+        //indexers
+
+        //access-specifiers
+        //1.public
+        //2.protected
+        //3.internal
+        //4.private
+        //5.private protected
+        //6.internal protected
+
+        public int a,b;
+    }
+    class Class4
+    {
+        public static void Main()
+        {
+            //<class Name> objectname =new //();
+            Student obj = new Student();
+            obj.a = 10;
+            obj.b = 20;
+            Console.WriteLine(obj.a + obj.b);
+        }
+    }
+}
