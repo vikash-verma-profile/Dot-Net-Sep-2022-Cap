01fdf3b7930791dd170cc925774ed9a070d6c971
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-09-05 17:07:41 +0530

    code

---
+++ LABS/Day-4/Day-4/Class7.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_4
+{
+    /// <summary>
+    /// Types of Inheritance
+    /// 1.Single level Inheritance
+    /// 2.Multi Level Inheritance
+    /// 3.Multiple Inheritance
+    /// 4.Herarical Inheritance
+    /// 5.Hybrid Inheritance
+    /// </summary>
+    class Class7
+    {
+    }
+}
