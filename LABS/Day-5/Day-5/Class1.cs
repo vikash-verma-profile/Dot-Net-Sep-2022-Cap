43fe5333dae2b2218869616f134217eb0675370f
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-09-06 10:00:26 +0530

    added to string ovveride

---
+++ LABS/Day-5/Day-5/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_5
+{
+    /// <summary>
+    /// interfaces
+    /// abstarct
+    /// 
+    /// static
+    /// virtual
+    /// base
+    /// override
+    /// operator
+    /// sealed
+    /// </summary>
+    /// 
+   abstract class abc
+    {
+        abstract public void print();
+        public void show()
+        {
+            Console.WriteLine("asdasd");
+        }
+    }
+    class ABC2 : abc
+    {
+        int a=1;
+        int b=2;
+        public override void print()
+        {
+            Console.WriteLine("I am print Method");
+        }
+        public override string ToString()
+        {
+            return "a= "+a+"b= "+b;
+        }
+
+    }
+    class Class1
+    {
+        public static void Main()
+        {
+            //1.ToString();
+            ABC2 abc = new ABC2();
+            string sampl=abc.ToString();
+            Console.WriteLine(sampl);
+        }
+    }
+}
---
+++ LABS/Day-5/Day-5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Day_5
+{
+    /// <summary>
+    /// jagged array's
+    /// array of array's
+    /// </summary>
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            //[] 1d
+            //[,] 2d
+            //[][]=>jagged array
+
+            int[][] samplearray = new int[][] 
+            {
+                new int[]{1,2,3},
+                new int[]{1,2,3,4},
+                new int[]{ 1,2,3,4,5}
+            };
+
+            int[][] jagged = new int[4][];
+            jagged[0] = new int[] { 1, 2, 3 };
+            jagged[1] = new int[] { 1, 2, 3,4 };
+            jagged[2] = new int[] { 1, 2, 3,4,5 };
+            jagged[3] = new int[] { 1, 2, 3 ,4,5,6};
+
+            for (int i = 0; i < jagged.Length; i++)
+            {
+                for (int k = 0; k < jagged[i].Length; k++)
+                {
+                    Console.Write(jagged[i][k]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
