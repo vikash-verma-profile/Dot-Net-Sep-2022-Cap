c824a4637db9da0542d1ee2ad690ccfe71d11190
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-09-07 09:01:51 +0530

    aded instance variables

---
+++ LABS/Day-6/Day-6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Day_6
+{
+    //static
+    //local static and instance variables
+
+    class B
+    {
+        //instance variable
+        int a;
+        public B(int a)
+        {
+            //local scope
+            this.a = a;//this is the keyword that will point to current class object
+        }
+        public void print()
+        {
+            Console.WriteLine("Value of a is "+a);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            B b = new B(1);
+            b.print();
+            B b1 = new B(2);
+            b1.print();
+        }
+    }
+}
