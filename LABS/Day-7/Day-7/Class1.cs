2eac0ca45825b9d54b6b2ee53bbd7d05f32e717b
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-09-08 09:06:43 +0530

    added static

---
+++ LABS/Day-7/Day-7/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_7
+{
+    //static keyword
+    /// <summary>
+    /// classes
+    /// variables
+    /// methods
+    /// 
+    /// constructors
+    /// </summary>
+    /// 
+
+    //static class cannot have non static members and methods
+    static class A
+    {
+        public static int a;
+        public static void Print()
+        {
+            Console.WriteLine(a);
+        }
+    }
+    class B
+    {
+        public static int a;
+        public int b;
+        static B()
+        {
+            a = 1; //only static members can be intiated
+                   // b = 3;
+            Console.WriteLine("Hi i am from static ");
+        }
+        public B()
+        {
+            b = 1;
+            a = 2;
+            Console.WriteLine("Hi i am from default constructor");
+        }
+        public void Print()
+        {
+            Console.WriteLine("a= "+a+"b= "+b);
+        }
+        public static void Show()
+        {
+            Console.WriteLine("a= " + a );
+        }
+    }
+    class Class1
+    {
+        public static void Main()
+        {
+             B b = new B();
+            //b.Print();
+            //B.Show();
+        }
+    }
+}
---
+++ LABS/Day-7/Day-7/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Day_7
+{
+
+    //static
+    //properties and indexers
+    //type of constructors
+    //structs and enums
+    //collections
+    //exception handling
+    //grabage collection
+    //file handling
+    //threads
+    //partial classes and extension methods
+    //Delegates
+    //new features in c#8.0
+    //git commands
+
+    class SampleA
+    {
+        int a;
+        public override string ToString()
+        {
+            return "value of a is "+a;
+        }
+    }
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            SampleA a = new SampleA();
+            string value=a.ToString();
+            Console.WriteLine(value);
+        }
+    }
+}
