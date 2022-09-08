770eb83c10d6e4439c39f46db826be039ccf8c7c
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-09-08 17:32:13 +0530

    added structure

--- LABS/Day-7/Day-7/Class9.cs
+++ LABS/Day-7/Day-7/Class9.cs
@@ -1,62 +1,46 @@
 ﻿using System;
-using System.Collections;
+using System.Collections.Generic;
 using System.Text;
 
 namespace Day_7
 {
-
-    //collections
-    /*
-     * non -generic collections   Generic collections
-     * ArrayList                    List
-     * HashTable                    Dictionary
-     * SortedList                   SortedList
-     * Stack                        Stack
-     * queue                        queue
-     */
-
-
-
-    class Class8
+    class Class9
     {
-        public static void Main9()
+        public static void Main()
         {
-            Console.WriteLine("========ArrayList==============");
-            ArrayList al = new ArrayList();
-            string str = "Vikash";
-            int x = 7;
-            DateTime d = DateTime.Now;
-            al.Add(str);
-            al.Add(x);
-            al.Add(d);
-            al.Remove(x);
+            Console.WriteLine("========List==============");
+            List<int> al = new List<int>();
+            al.Add(1);
+            al.Add(2);
+            al.Add(3);
+            al.Remove(1);
             foreach (var item in al)
             {
                 Console.WriteLine(item);
             }
-            Console.WriteLine("========HashTable==============");
-            Hashtable ht = new Hashtable();
-            ht.Add("asp","asp.net");
+            Console.WriteLine("========Dictionay==============");
+            Dictionary<string,string> ht = new Dictionary<string, string>();
+            ht.Add("asp", "asp.net");
             ht.Add("cs", "cs.net");
             ht.Add("vb", "vb.net");
-            foreach (DictionaryEntry item in ht)
+            foreach (KeyValuePair<string,string> item in ht)
             {
-                Console.WriteLine(item.Key+"===>"+item.Value);
+                Console.WriteLine(item.Key + "===>" + item.Value);
             }
             Console.WriteLine("========SortedList==============");
-            SortedList sl = new SortedList();
-            sl.Add("ora","oracle");
+            SortedList<string,string> sl = new SortedList<string, string>();
+            sl.Add("ora", "oracle");
             sl.Add("asp", "asp.net");
             sl.Add("cs", "cs.net");
             sl.Add("vb", "vb.net");
 
-            foreach (DictionaryEntry item in sl)
+            foreach (KeyValuePair<string, string> item in sl)
             {
                 Console.WriteLine(item.Key + "===>" + item.Value);
             }
 
             Console.WriteLine("========Stack==============");
-            Stack st = new Stack();
+            Stack<string> st = new Stack<string>();
             st.Push("oracle");
             st.Push("asp.net");
             st.Push("cs.net");
@@ -68,7 +52,7 @@ namespace Day_7
             }
 
             Console.WriteLine("========Queues==============");
-            Queue q = new Queue();
+            Queue<string> q = new Queue<string>();
             q.Enqueue("oracle");
             q.Enqueue("asp.net");
             q.Enqueue("cs.net");
