0ba18f96339fb7e6557b6de4a34da98378aa1c8e
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-09-14 09:05:34 +0530

    added dll command

--- DB-Queries/SQLQuery1.sql
+++ DB-Queries/SQLQuery1.sql
@@ -11,6 +11,8 @@ select * from Sample
 select id,TEXT from Sample
 insert into Sample values(1,'Vikash')
 insert into Sample values(2,'Vikash')
+insert into Sample values(3,'Vikash')
+insert into Sample values(4,'Vikash')
 select * from Sample where id=1
 select * from sample where id in (1,2)
 select * from sample where id not in (1)
@@ -18,3 +20,12 @@ select * from sample where id not in (1)
 update SAMPLE set Text='suresh' where id=1
 
 delete  from sample where id=1
+
+drop table Sample;
+alter table sample drop column SAlary 
+alter table sample add  SAlary int
+select * from sample
+truncate table sample
+select * from Sample
+
+--truncate vs delete command
