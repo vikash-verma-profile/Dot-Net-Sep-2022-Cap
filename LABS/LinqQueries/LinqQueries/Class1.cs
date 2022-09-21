6d16af677f710589947a5f3a15a08d589e90def9
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-09-21 14:50:07 +0530

    add

---
+++ LABS/LinqQueries/LinqQueries/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LinqQueries
+{
+    //ofType
+    class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int StudentAge { get; set; }
+    }
+    class Class1
+    {
+        public static void Main()
+        {
+
+            IList mixedList = new ArrayList();
+            mixedList.Add(0);
+            mixedList.Add("Two");
+            mixedList.Add("Three");
+            mixedList.Add(3);
+            mixedList.Add(new Student() { StudentID=1,StudentName="Vikash"});
+
+            var stringResult = from s in mixedList.OfType<string>() select s;
+            var stringResult1 = mixedList.OfType<string>();
+
+            Console.WriteLine("=======OfType=========");
+            Console.WriteLine("Linq ");
+            foreach (var item in stringResult)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Linq to entity");
+            foreach (var item in stringResult1)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
--- LABS/LinqQueries/LinqQueries/Program.cs
+++ LABS/LinqQueries/LinqQueries/Program.cs
@@ -3,15 +3,27 @@ using System;
 
 namespace LinqQueries
 {
+
+    //where
+    //join
+
+    //select
+
+    //first and firstordefault
+    //last and lastordefault
+    //elementat and elementatDefault
+    //single and singleordefault
     class Program
     {
-        static void Main(string[] args)
+        static void Main1(string[] args)
         {
             TestDBContext db = new TestDBContext();
             foreach (var item in db.TblEmployees)
             {
                 Console.WriteLine(item.EmployeeName);
             }
+
+
         }
     }
 }
