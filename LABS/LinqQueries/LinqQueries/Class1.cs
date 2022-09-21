9c115cd3892365033875f1d6d379c4a072e14ebe
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-09-21 15:22:44 +0530

    add

--- LABS/LinqQueries/LinqQueries/Class1.cs
+++ LABS/LinqQueries/LinqQueries/Class1.cs
@@ -40,6 +40,31 @@ namespace LinqQueries
                 Console.WriteLine(item);
             }
 
+            IList<Student> studentList = new List<Student>() { 
+            
+                new Student(){StudentID=1,StudentName="john",StudentAge=18},
+                  new Student(){StudentID=2,StudentName="Steve",StudentAge=15},
+                    new Student(){StudentID=3,StudentName="Bill",StudentAge=25},
+                     new Student(){StudentID=4,StudentName="Ram",StudentAge=20},
+                      new Student(){StudentID=5,StudentName="Ron",StudentAge=19}
+            };
+
+            var orderbyRasult = from s in studentList orderby s.StudentName select s;
+            var orderbyResult2 = studentList.OrderBy(x => x.StudentName);
+
+            Console.WriteLine("============Order by=======");
+            foreach (var item in orderbyRasult)
+            {
+                Console.WriteLine(item.StudentName);
+            }
+            foreach (var item in orderbyResult2)
+            {
+                Console.WriteLine(item.StudentName);
+            }
+
+            var orderbyRasultDesc = from s in studentList orderby s.StudentName descending select s;
+            var orderbyResultDesc2 = studentList.OrderByDescending(x => x.StudentName);
+
         }
     }
 }
