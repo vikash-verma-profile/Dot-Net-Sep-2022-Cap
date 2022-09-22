ffc1fba43dcf83143ba403325f849e8914b05f0d
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-09-22 11:37:59 +0530

    add code

---
+++ LABS/CustomerAPI/CustomerAPI/Models/CustomerDb1Context.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace CustomerAPI.Models
+{
+    public partial class CustomerDb1Context : DbContext
+    {
+        public CustomerDb1Context()
+        {
+        }
+
+        public CustomerDb1Context(DbContextOptions<CustomerDb1Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TblCustomer> TblCustomers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-CUO3FKB;Initial Catalog=CustomerDb1;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<TblCustomer>(entity =>
+            {
+                entity.ToTable("tblCustomer");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.CustomerAmount).HasColumnType("decimal(5, 2)");
+
+                entity.Property(e => e.CustomerCode).HasMaxLength(200);
+
+                entity.Property(e => e.CustomerName).HasMaxLength(200);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ LABS/CustomerAPI/CustomerAPI/Models/TblCustomer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace CustomerAPI.Models
+{
+    public partial class TblCustomer
+    {
+        public int Id { get; set; }
+        public string CustomerCode { get; set; }
+        public string CustomerName { get; set; }
+        public decimal? CustomerAmount { get; set; }
+    }
+}
