003ecf1096d165fe056cfe2e48cec749b8a61b49
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-09-23 14:50:18 +0530

    added code

---
+++ LABS/CustomerLogin/CustomerLogin/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using CustomerLogin.ViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerLogin.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel loginViewModel)
+        {
+            
+            return Ok(new {Message= "Successfully login"});
+        }
+    }
+}
---
+++ LABS/CustomerLogin/CustomerLogin/Models/LoginDBContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace CustomerLogin.Models
+{
+    public partial class LoginDBContext : DbContext
+    {
+        public LoginDBContext()
+        {
+        }
+
+        public LoginDBContext(DbContextOptions<LoginDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TblLogin> TblLogins { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-CUO3FKB;Initial Catalog=LoginDB;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<TblLogin>(entity =>
+            {
+                entity.ToTable("tblLogin");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.FirstName).HasMaxLength(200);
+
+                entity.Property(e => e.LastName).HasMaxLength(200);
+
+                entity.Property(e => e.Password).HasMaxLength(200);
+
+                entity.Property(e => e.UserName).HasMaxLength(200);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
